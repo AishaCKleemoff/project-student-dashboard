e2694cf1b5e63c826bd037f7d48aa75e53ac6c15
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-08-08 20:45:39 -0400

    Create Student List and Student Card Components.

---
+++ src/Components/StudentList.jsx
@@ -0,0 +1,53 @@
+import StudentCard from "./StudentCard";
+
+export default function StudentList({ data, students, cohortName }) {
+  let result = student.filter(
+    (student) => student.cohort.cohortCode === cohortName
+  );
+  return (
+    <div className="student-list">
+      {cohortName === "All Students" ? (
+        <>
+          <h2 className="color-student-list">All Students</h2>
+          <br />
+        </>
+      ) : (
+        <>
+          <h2 className="color-student-list">
+            {cohortName.split(/[0-9]/g)} {cohortName.slice(-4)}
+          </h2>
+          <br />
+        </>
+      )}
+
+      <p>
+        <span className="student-total-color">
+          Total Students:{" "}
+          {cohortName === "AllStudents" ? (
+            <span>{data.length}</span>
+          ) : (
+            <span>{result.length}</span>
+          )}
+        </span>
+      </p>
+
+      {cohortName === "All Student"
+        ? students.map((student) => {
+            return (
+              <div key={student.id}>
+                <StudentCard key={student.id} student={student} />
+              </div>
+            );
+          })
+        : students
+            .filter((student) => student.cohort.cohortCode === cohortName)
+            .map((student) => {
+              return (
+                <div key={student.id}>
+                  <StudentCard key={student.id} student={student} />
+                </div>
+              );
+            })}
+    </div>
+  );
+}
